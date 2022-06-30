99fd0aa0ccd58c411d232c06ec9108a8f371d251
Parker Scanlon <pscanlo@amazon.com>
2022-06-30 12:50:56 -0400

    Adding pipeline

--- bin/example-app.ts
+++ bin/example-app.ts
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
+import { ExamplePipelineStack } from '../lib/pipeline-stack';
 import { ExampleAppStack } from '../lib/example-app-stack';
 
 const app = new cdk.App();
-new ExampleAppStack(app, 'ExampleAppStack', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-});
+new ExamplePipelineStack(app, 'MyExamplePipelineStack');
