bbf52234db89a5c314419fbef6de4bfb28d0ad4c
Parker Scanlon <pscanlo@amazon.com>
2022-07-05 10:45:51 -0400

    Connection to Github

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -3,19 +3,23 @@ import * as codecommit from 'aws-cdk-lib/aws-codecommit';
 import { CodeBuildStep, CodePipelineSource, CodePipeline } from 'aws-cdk-lib/pipelines';
 import { ExamplePipelineStage } from './pipeline-stage';
 import { Construct } from 'constructs';
+import { CodeStarConnectionsSourceAction } from 'aws-cdk-lib/aws-codepipeline-actions';
 
 export class ExamplePipelineStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
         // Creates a CodeCommit repository called 'ExampleRepo'
-        const repo = codecommit.Repository.fromRepositoryName(this, 'ExampleRepo', 'ExampleRepo');
+        //const repo = codecommit.Repository.fromRepositoryName(this, 'ExampleRepo', 'ExampleRepo');
         
         // Pipeline code goes here
         const pipeline = new CodePipeline(this, 'Pipeline', {
             pipelineName: 'ExamplePipeline',
             synth: new CodeBuildStep('SynthStep', {
-                input: CodePipelineSource.gitHub('scanlonp/ReferenceApp', 'master'),
+                input: CodePipelineSource.connection('scanlonp/ReferenceApp', 'master', {
+                    connectionArn: 'arn:aws:codestar-connections:us-east-1:111279636657:connection/ee55ed61-4892-4008-a32b-a7c549c27ca8',
+                }),
+                //input: CodePipelineSource.gitHub('scanlonp/ReferenceApp', 'master'),
                 //input: CodePipelineSource.codeCommit(repo, 'master'),
                 installCommands: [
                     'npm install -g aws-cdk'
@@ -26,6 +30,7 @@ export class ExamplePipelineStack extends cdk.Stack {
                     ' npx cdk synth'
                 ],
             }),
+            
             
             // to try to build the ecr deploy lambda at synth time
             dockerEnabledForSynth: true,
